4258c9e027871e2a70c53cddc84a3d6075030c9d
hyunjung <khj1259@gmail.com>
2019-01-28 15:58:45 +0900

    swing

---
+++ FirstProject/src/com/gui/SwingTest.java
@@ -0,0 +1,73 @@
+package com.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class SwingTest {
+	
+	//JTextArea, JList, JTable 는 JScrollPane 위에 올려야 됨
+	//field
+	JFrame f; // 창
+	JButton ok, cancel; // 버튼
+	JLabel la; // 글자
+	JTextArea ta; // 여러줄 입력칸
+	JTextField tf; // 한줄 입력칸
+	JList list; // 리스트
+	
+	public SwingTest() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		//화면 만들기 작업
+		f = new JFrame("swing test");
+		f.setLayout(new BorderLayout());//layout manager 지정
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//창 닫기 버튼 클릭시 프로그램 종료
+		ok = new JButton("ok");
+		cancel = new JButton("cancel");
+		la = new JLabel("Swing Test", JLabel.CENTER);//화면에서의 글자
+		Font font = new Font("맑은고딕", Font.BOLD, 50);
+		la.setFont(font);
+		la.setForeground(Color.PINK);
+		
+		ta = new JTextArea(5, 30);
+		//JScrollPane : 스크롤바가 있는 pane
+		JScrollPane pane = new JScrollPane(ta);//JScrollPane 위에 ta를 올림
+		
+		tf = new JTextField(30);
+		String[] season = {"spring", "summer", "fall", "winter"};
+		list = new JList<>(season);
+		list.setVisibleRowCount(3);
+		JScrollPane pane2 = new JScrollPane(list);
+		
+		JPanel panel = new JPanel(new FlowLayout());//JFrame 보다 작은 컨테이너(내부에 다른 화면 구성요소를 담을 수 있음)
+		panel.add(ok);
+		panel.add(cancel);
+		
+		Container c = f.getContentPane();
+		c.add(la, "North");
+		c.add(panel, "South");
+//		c.add(cancel);
+		c.add(pane, "Center");
+//		c.add(tf);
+//		c.add(pane2);
+		
+		f.setSize(500, 500);
+		f.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new SwingTest();
+	}
+
+}
